580b47e5fc30937802cfdb765acd2885e5a6d3dc
Marcelo <marcelocdias93@gmail.com>
2021-03-28 19:07:22 -0300

    Clean compiler controller home action

--- app/controllers/compiler_controller.rb
+++ app/controllers/compiler_controller.rb
@@ -1,9 +1,4 @@
 class CompilerController < ActionController::Base
   def home
-    params = { token_class: 'id',
-               lexeme: 'test',
-               type: nil }
-
-    @token = Token.new(params)
   end
 end
