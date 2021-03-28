233ddf916d49fd6f76c7cfa45665ec41c1ccf227
Marcelo <marcelocdias93@gmail.com>
2021-03-28 19:00:34 -0300

    Add order attribute to token model

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,7 +1,8 @@
 class Token
-	attr_accessor :token_class, :lexeme, :type
+	attr_accessor :index, :token_class, :lexeme, :type
 
 	def initialize args
+    @order = args[:order]
     @token_class = args[:token_class]
     @lexeme = args[:lexeme]
     @type = args[:type]
