9719b5b69e2f726fa44ef78a068f03262ba443bf
Marcelo <marcelocdias93@gmail.com>
2021-03-28 18:35:52 -0300

    Create token model

---
+++ app/models/token.rb
@@ -0,0 +1,9 @@
+class Token
+	attr_accessor :token_class, :lexeme, :type
+
+	def initialize args
+    @token_class = args[:token_class]
+    @lexeme = args[:lexeme]
+    @type = args[:type]
+	end
+end
