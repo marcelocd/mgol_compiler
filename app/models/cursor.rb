e3a9cb8ea3674e6d425a8d09dc3830ace6f158da
Marcelo <marcelocdias93@gmail.com>
2021-03-29 21:30:18 -0300

    Create cursor model

---
+++ app/models/cursor.rb
@@ -0,0 +1,19 @@
+class Cursor
+  attr_accessor :index, :line, :column
+
+  def initialize
+		@index = 0
+		@line = 1
+		@column = 1
+	end
+
+	def update_cursor_position current_character
+		@index += 1
+    if current_character.match(/\n/)
+      @line += 1
+      @column = 1
+    else
+      @column += 1
+    end
+	end
+end
