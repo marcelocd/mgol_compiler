bd012d8a3786bbff9ecf1e74db44cd52ba94ca45
Marcelo <marcelocdias93@gmail.com>
2021-03-30 00:25:59 -0300

    Implement lexical analyzer logic

--- app/models/lexical_analyzer.rb
+++ app/models/lexical_analyzer.rb
@@ -1,93 +1,137 @@
 class LexicalAnalyzer
-  INITIAL_STATE = 's0'
-	ID_STATE = 's9'
-  EOF_STATE = 's12'
-	ERROR_STATE = 's28'
+  attr_accessor :source_code
 
-  def transition_table
-		{
-      INITIAL_STATE => {
-				"D"=> "s1",
-				"\"" => "s7",
-				"L"=> "s9",
-				"{"=> "s10",
-				"EOF" => EOF_STATE,
-				">" => "s13",
-				"=" => "s15",
-				"<" => "s16",
-				"+" => "s20",
-				"-" => "s21",
-				"*" => "s22",
-				"/" => "s23",
-				"(" => "s24",
-				")" => "s25",
-				";" => "s26",
-				"," => "s27",
-				"ERROR" => "s28"
-			},
-			"s1" => {
-				"D" => "s1",
-        "." => "s2",
-        "e" => "s4",
-				"E" => "s4"
-			},
-			"s2" => {
-				"D"   => "s3"
-			},
-			"s3" => {
-				"D"   => "s3",
-				"e"   => "s4",
-				"E"   => "s4"
-			},
-			"s4" => {
-				"+"   => "s5",
-				"-"   => "s5",
-				"D"   => "s6"
-			},
-			"s5" => {
-				"D"   => "s6"
-			},
-			"s6" => {
-				"D"   => "s6"
-			},
-			"s7" => {
-        "NON_DOUBLE_QUOTES" => "s7",
-				"\"" => "s8"
-			},
-			"s8" => { },
-			ID_STATE => {
-				"D" => ID_STATE,
-				"L" => ID_STATE,
-				"_" => ID_STATE
-			},
-			"s10" => {
-        "NON_CLOSING_BRACES" => "s10",
-				"}" => "s11"
-			},
-			"s11" => { },
-			EOF_STATE => { },
-			"s13" => {
-				"=" => "s14"
-			},
-			"s14" => { },
-			"s15" => { },
-			"s16" => {
-				">" => "s17",
-				"-" => "s18",
-				"=" => "s19"
-			},
-			"s17" => { },
-			"s18" => { },
-			"s19" => { },
-			"s20" => { },
-			"s21" => { },
-			"s22" => { },
-			"s23" => { },
-			"s24" => { },
-			"s25" => { },
-			"s26" => { },
-			"s27" => { },
-			"s28" => { }
-		}
+  def initialize args
+    @source_code = args[:source_code]
+    @dfa = Dfa.new
+    @cursor = Cursor.new
+    @current_state = Dfa::INITIAL_STATE
+    @current_character = source_code[cursor.index]
+    @buffer = ''
+  end
+
+  def scan
+    jump_ignorable_characters
+
+    token = get_next_token
+
+    # Implement error tokens treatment!
+    reset_current_state
+    clean_buffer
+    token
+  end
+
+  private
+
+  def get_next_token
+    if eof_has_been_reached?
+      update_current_state
+      return token_by_current_state
+    end
+
+    process_lexeme
+    token_by_current_state
+  end
+
+  def process_lexeme
+    while (!current_character_is_ignorable? &&
+           !current_character_is_a_delimiter? &&
+           !eof_has_been_reached?)
+      process_current_character
+      cursor.update_position(current_character)
+      update_current_character
+    end
+  end
+
+  def jump_ignorable_characters
+    while current_character_is_ignorable?
+      cursor.update_position(current_character)
+      update_current_character
+    end
+  end
+
+  def current_character_is_ignorable?
+    return true if current_character.match(/[\s\t\n]/)
+    false
+  end
+
+  def current_character_is_a_delimiter?
+    return true if current_character.match(/[,\.]/)
+    false
+  end
+
+  def eof_has_been_reached?
+    current_character.nil?
+  end
+
+  def process_current_character
+    add_current_character_to_buffer
+    update_current_state
+  end
+
+  def token_by_current_state
+    token_class = nil
+
+    case current_state
+    when Dfa::INITIAL_STATE
+    when 's1', 's3', 's6'
+      token_class = 'Num'
+    when 's8'
+      token_class = 'Literal'
+    when Dfa::ID_STATE
+      token_class = 'id'
+    when 's11'
+      token_class = 'Comentário'
+    when Dfa::EOF_STATE
+      token_class = 'EOF'
+    when 's13', 's14', 's15', 's16', 's17', 's19'
+      token_class = 'OPR'
+    when 's18'
+      token_class = 'RCB'
+    when 's20', 's21', 's22', 's23'
+      token_class = 'OPM'
+    when 's24'
+      token_class = 'AB_P'
+    when 's25'
+      token_class = 'FC_P'
+    when 's26'
+      token_class = 'PT_V'
+    when 's27'
+      token_class = 'Vir'
+    else
+      token_class = 'ERRO'
+    end
+
+    Token.new(token_class: token_class, lexeme: buffer)
 	end
+
+  def add_current_character_to_buffer
+    buffer += current_character
+  end
+
+  def update_current_state
+    current_state = next_state
+  end
+
+  def update_current_character
+    current_character = source_code[cursor.index]
+  end
+
+  def next_state
+    transition_key = dfa.transition_key(current_character, current_state)
+
+    if dfa.transition_table[current_state][transition_key].present?
+      dfa.transition_table[current_state][transition_key]
+    else
+      Dfa::ERROR_STATE
+    end
+  end
+
+  def reset_current_state
+    current_state = Dfa::INITIAL_STATE
+  end
+
+  def clean_buffer
+    buffer = ''
+  end
 end
