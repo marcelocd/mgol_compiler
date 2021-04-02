063db92bedb9b18e36925c549b55a5c001d9d3b7
Marcelo <marcelocdias93@gmail.com>
2021-04-02 19:53:32 -0300

    Refactoring

--- app/controllers/lexical_analyzer_controller.rb
+++ app/controllers/lexical_analyzer_controller.rb
@@ -1,7 +1,27 @@
 class LexicalAnalyzerController < ActionController::Base
   def index
-    source_code = params[:source_code] || ''
+    source_code
+    lexical_analyzer
+    tokens
+  end
 
-    @lexical_analyzer = LexicalAnalyzer.new(source_code: source_code)
+  private
+
+  def source_code
+    @source_code ||= params[:source_code] || ''
+  end
+
+  def lexical_analyzer
+    @lexical_analyzer ||= LexicalAnalyzer.new(source_code: source_code)
+  end
+
+  def tokens
+    return @tokens = [] if source_code.blank?
+    
+    @tokens = Array.new
+    loop do
+      @tokens << lexical_analyzer.scan
+      return @tokens if lexical_analyzer.eof_has_been_reached?
+    end
   end
 end
