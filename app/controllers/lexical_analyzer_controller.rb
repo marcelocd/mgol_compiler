4ed498511efd18cfccd525982f0cf0f83dabfc74
Marcelo <marcelocdias93@gmail.com>
2021-04-11 20:21:13 -0300

    Read source code

--- app/controllers/lexical_analyzer_controller.rb
+++ app/controllers/lexical_analyzer_controller.rb
@@ -11,7 +11,18 @@ class LexicalAnalyzerController < ActionController::Base
   private
 
   def source_code
-    @source_code ||= remove_double_line_breaks(params[:source_code]) || ''
+    # OPTION 1 -------------
+    file = File.open(source_code_path)
+		@source_code = file.read
+		file.close
+    @source_code
+    
+    # OPTION 2 -------------
+    # @source_code ||= remove_double_line_breaks(params[:source_code]) || ''
+  end
+
+  def source_code_path
+    Rails.root.join('mgol_example_code.txt').to_s
   end
 
   def lexical_analyzer
